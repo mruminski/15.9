e4f305d60e8f4f37f8bab4add469cca471f0727f
Mateusz Rumiński <matt.ruminski@protonmail.com>
2018-09-30 16:31:09 +0200

    Add UsersList component

--- js/script.js
+++ js/script.js
@@ -37,6 +37,20 @@ class App extends React.Component {
   }
 }
 
+class UsersList extends React.Component {
+  get users() {
+    return this.props.users.map(user => <User key={user.id} user={user}/>);
+  }
+  
+  render() {
+    return (
+      <div>
+        {this.users}
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(
   <App />,
   document.querySelector('#root')
