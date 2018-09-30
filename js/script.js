e601d33c236987d677658858f9c1a787542cd632
Mateusz Rumiński <matt.ruminski@protonmail.com>
2018-09-30 16:34:46 +0200

    Add User component

--- js/script.js
+++ js/script.js
@@ -41,11 +41,22 @@ class UsersList extends React.Component {
   get users() {
     return this.props.users.map(user => <User key={user.id} user={user}/>);
   }
-  
+
   render() {
     return (
       <div>
         {this.users}
+      </div>
+    );
+  }
+}
+
+class User extends React.Component {
+  render() {
+    return (
+      <div>
+        <img src={this.props.user.avatar_url} style={{maxWidth: '100px'}}/>
+        <a href={this.props.user.html_url} target='_blank'>{this.props.user.login}</a>
       </div>
     );
   }
